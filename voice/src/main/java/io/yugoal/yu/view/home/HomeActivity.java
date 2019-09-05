c34f1a8ea2a7ab03a9d1cb1d7f462ca4a63739b1
caoyu <1127caoyu@gmail.com>
2019-09-05 17:35:41 +0800

    项目基础架构搭建完成

---
+++ voice/src/main/java/io/yugoal/yu/view/home/HomeActivity.java
@@ -0,0 +1,16 @@
+package io.yugoal.yu.view.home;
+
+import androidx.fragment.app.FragmentActivity;
+
+import io.yugoal.yu.model.CHANNEL;
+
+/**
+ * @author caoyu
+ * date 2019/9/4
+ * 首页
+ */
+public class HomeActivity extends FragmentActivity {
+
+    private static final CHANNEL[] CHANNELS =
+            new CHANNEL[]{CHANNEL.MY, CHANNEL.DISCORY, CHANNEL.FRIEND};
+}
